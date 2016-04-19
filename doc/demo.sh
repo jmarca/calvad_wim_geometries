325640746f8706fb3b27293e03e22cc709cf259a
James E. Marca <james@activimetrics.com>
2016-04-19 08:39:13 -0700

    update demo script

--- doc/demo.sh
+++ doc/demo.sh
@@ -5,7 +5,12 @@
 
 # note the db name, and change below
 
-sqitch target add brokendb db:pg://slash@127.0.0.1/8_22_43
+sqitch target add brokendb db:pg://slash@127.0.0.1/wim8_22_43
 sqitch engine add pg brokendb
 
 sqitch add fix_4269_geoms -m 'Fix metadata vs geom mismatch'
+
+show the edited file
+
+run it
+run the test
