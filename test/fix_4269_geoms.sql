48b0b649e3fb624775358f99f83e83da6e4f9e36
James E. Marca <james@activimetrics.com>
2016-04-19 10:29:10 -0700

    create a test for fixed wim_points_4269
    copy in from test_geometry_vs_metadata

--- test/fix_4269_geoms.sql
+++ test/fix_4269_geoms.sql
@@ -8,5 +8,23 @@ SELECT no_plan();
 
 SELECT pass('Test fix_4269_geoms!');
 
+-- do the geometries actually match the metadata in all cases?
+PREPARE build_geoms_4269 AS
+  SELECT ST_GeomFromEWKT('SRID=4269;POINT('||a.longitude||' '||a.latitude||')')
+  FROM wim_stations a
+  ORDER BY site_no;
+
+PREPARE existing_geoms_4269 AS
+  SELECT b.geom
+  FROM wim_points_4269 a
+  JOIN geom_points_4269 b USING (gid)
+  ORDER BY wim_id;
+
+-- expect that the metadata matches the stored 4269 geoms
+SELECT results_eq(
+    'build_geoms_4269',
+    'existing_geoms_4269'
+);
+
 SELECT finish();
 ROLLBACK;
