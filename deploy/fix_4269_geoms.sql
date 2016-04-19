1e04d9751d5d11cc543672353a8de680965dce21
James E. Marca <james@activimetrics.com>
2016-04-19 08:27:36 -0700

    initial conditions step 3 of sqitch demo

---
+++ deploy/fix_4269_geoms.sql
@@ -0,0 +1,7 @@
+-- Deploy calvad_wim_geometries:fix_4269_geoms to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ revert/fix_4269_geoms.sql
@@ -0,0 +1,7 @@
+-- Revert calvad_wim_geometries:fix_4269_geoms from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
