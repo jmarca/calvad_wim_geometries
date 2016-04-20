f4b657e96dea99320873899ae685b126398371c1
James E. Marca <james@activimetrics.com>
2016-04-19 20:04:19 -0700

    improve demo for demo

--- doc/demo.sh
+++ doc/demo.sh
@@ -1,12 +1,15 @@
-# Part 2
+# Part 1
 
 # create a test database
 ./node_modules/.bin/mocha test/test_deploy.js --timeout 0
 
-# note the db name, and change below
+# note the db name, and search replace with broken below
 
-sqitch target add brokendb db:pg://slash@127.0.0.1/wim8_22_43
-sqitch engine add pg brokendb
+sqitch target add broken db:pg://slash@127.0.0.1/broken
+sqitch engine add pg broken
+
+# Part 2
+
 
 sqitch add fix_4269_geoms -m 'Fix metadata vs geom mismatch'
 
@@ -20,7 +23,7 @@ sqitch add fix_4326_geoms -m 'Fix reprojected geometry mismatch'
 
 # Step 6 ish  show the test, test will fail
 
-pg_prove -d wim8_22_43 test/fix_4326_geoms.sql
+pg_prove -d wimbroken test/fix_4326_geoms.sql
 
 # step 7, have edited the deploy,
 
@@ -30,4 +33,4 @@ sqitch deploy
 
 # test now passes
 
-pg_prove -d wim8_22_43 test/fix_4326_geoms.sql
+pg_prove -d wimbroken test/fix_4326_geoms.sql
